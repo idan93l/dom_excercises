cb80fa1297c3283149a971a7b647370d7ba7d59a
idan.93l <idan.93lev@gmail.com>
2021-11-24 18:37:37 +0200

    DO

---
+++ 19.1-dom_walk/script.js
@@ -0,0 +1,21 @@
+const start = document.querySelector(`ul`);
+
+start.querySelector(`.start-here`).textContent = `main title`;
+
+const sub4 = document.createElement(`li`)
+
+start.querySelector(`ul`).appendChild(sub4);
+
+sub4.textContent = `sub title 4`;
+
+start.removeChild(start.lastElementChild);
+
+const title = document.querySelector(`title`);
+
+title.innerHTML = `Master Of The Dom`;
+
+// document.querySelector(`p`).textContent = `I am Iron Man!!!!!!!`
+
+const p = document.querySelector(`p`);
+
+p.textContent = `I am Iron Man!!!!!!!`
