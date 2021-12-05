8b70e69ba917af27cd1a6ff1bd804d5e2766c8a6
idan.93l <idan.93lev@gmail.com>
2021-12-05 14:42:22 +0200

    Is Square

---
+++ 24.1/is-square.js
@@ -0,0 +1,18 @@
+function Square(a, b, c, d) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
+  this.d = d;
+}
+
+let sqr = new Square(2, 3, 2, 3);
+
+Square.prototype.isSquare = function() {
+    if(this.a === this.c && this.b === this.d) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+console.log(sqr.isSquare());
