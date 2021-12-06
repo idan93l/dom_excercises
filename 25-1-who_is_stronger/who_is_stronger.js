6ea641342342c5506dfbff3d79bfca073679e143
idan.93l <idan.93lev@gmail.com>
2021-12-06 12:42:22 +0200

    Who is stronger

---
+++ 25-1-who_is_stronger/who_is_stronger.js
@@ -0,0 +1,21 @@
+// There is a problem in getStrength() - "this" reffering to the global object instead of hero{}
+
+const hero = {
+  health: 5,
+  power: 68,
+  getStrength: function () {
+    if (this.health <= 5) {
+      return this.power - 10;
+    } else return this.power;
+  }
+};
+function whoIsStronger(getStrength) {
+  const myStrength = 82;
+  if (getStrength() < myStrength) {
+    return "I am stronger";
+  } else return "You are stronger";
+}
+
+const stronger = whoIsStronger(hero.getStrength.bind(hero));
+
+console.log(stronger);
