7ae79a491eab553c9b2c889f6814cda9bf74dec4
Idan Lev <idan.93lev@gmail.com>
2021-12-09 01:10:47 +0200

    Movie app

--- 30-2-movie_app/script.js
+++ 30-2-movie_app/script.js
@@ -1,37 +1,49 @@
 // https://api.github.com/movies/{moviename}
 const searchButton = document.querySelector(".searchButton");
-const profilePicture = document.querySelector(".profilePicture");
-const movieUrl = document.querySelector(".movieUrl");
-const repos = document.querySelector(".repos");
+const poster = document.querySelector(".poster");
+const movie = document.querySelector(".movie");
+const genre = document.querySelector(".genre");
+const year = document.querySelector(".year");
+const plot = document.querySelector(".plot");
+const director = document.querySelector(".director");
+const actors = document.querySelector(".actors");
 const headers = document.querySelector(".headers");
+const imdb = document.querySelector(".imdb");
+const tomatoes= document.querySelector(".tomatoes");
+const metacritic = document.querySelector(".metacritic");
 movieNameInput = document.querySelector(".movieName");
 movieNameInput.focus();
 
-const jokeGenerator = document.querySelector(".jokeGenerator");
-const URL = "https://api.jokes.one/jod";
-
-const getmovieName = () => {
-  movieToSearch = movieNameInput.value;
-  movieNameInput.value = "";
-  movieNameInput.focus();
-  return movieToSearch;
-};
+// const getmovieName = () => {
+//   movieToSearch = movieNameInput.value;
+//   movieNameInput.value = "";
+//   movieNameInput.focus();
+//   return movieToSearch;
+// };
 
 const fetchmovie = async () => {
-  let movieToSearch = getmovieName();
+  // let movieToSearch = getmovieName();
   try {
     const data = await (
-      await fetch(`https://api.github.com/movies/${movieToSearch}`)
+      await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=e96c0696`)
     ).json();
-    const reposData = await (await fetch(data.repos_url)).json();
-    profilePicture.innerHTML = `<img src="${data.avatar_url}">`;
-    movieUrl.innerHTML = `<a href="${data.html_url}" target="_blank">${movieToSearch}</a>`;
-    repos.innerHTML = `<a href="#">${reposData.length}</a>`;
+    // const reposData = await (await fetch(data.repos_url)).json();
+    poster.innerHTML = `<img src="${data.Poster}">`;
+    movie.textContent = `${data.Title}`;
+    genre.textContent = `${data.Genre}`;
+    year.textContent = `${data.Year}`;
+    plot.textContent = `${data.Plot.length} words`;
+    director.textContent = `${data.Director}`;
+    actors.textContent = `${data.Actors}`;
+    imdb.textContent = `${data.Ratings[0].Value}`;
+    tomatoes.textContent = `${data.Ratings[1].Value}`;
+    metacritic.textContent = `${data.Ratings[2].Value}`;
     headers.style.display = "block";
+    console.log(data);
   } catch (err) {
-    profilePicture.innerHTML = "";
-    movieUrl.innerHTML = "";
-    repos.innerHTML = "";
+    poster.innerHTML = "";
+    movie.textContent = "";
+    genre.textContent = "";
     headers.style.display = "none";
     console.log(err);
   }
