385ec27aa98d992a225b998afbdf0c00c0ff3a96
Idan Lev <idan.93lev@gmail.com>
2021-12-09 02:17:07 +0200

    Star Wars

--- 31-1-star_wars/script.js
+++ 31-1-star_wars/script.js
@@ -1,12 +1,17 @@
-const URL = "https://swapi.dev/api/people";
+const URL = `https://swapi.dev/api/people`;
 
 const getInfo = async () => {
   let allData = [];
   try {
     const data = await (await fetch(URL)).json();
-    // console.log(data);
-    const characters = data.results
-    for(let i = 0; i < characters.length; i++) {
+    const characters = data.results;
+    // document.body.innerHTML = `<div class="row">`;
+    // document.body.innerHTML += `<div class="row"><div>name</div>`;
+    // document.body.innerHTML += `<div>hair</div>`;
+    // document.body.innerHTML += `<div>height</div>`;
+    // document.body.innerHTML += `<div>planet name</div>`;
+    // document.body.innerHTML += `<div>plante population</div></div>`;
+    for (let i = 0; i < characters.length; i++) {
       let character = characters[i];
       let planet = await (await fetch(character.homeworld)).json();
       let obj = {
@@ -14,15 +19,21 @@ const getInfo = async () => {
         hair: character.hair_color,
         height: character.height,
         planetName: planet.name,
-        planetPopulation: planet.population
+        planetPopulation: planet.population,
       };
       allData.push(obj);
+      document.body.innerHTML += `<div class="row"><div>${character.name}</div>`
+      document.body.innerHTML += `<div>${character.hair_color}</div>`
+      document.body.innerHTML += `<div>${character.height}</div>`
+      document.body.innerHTML += `<div>${planet.name}</div>`
+      document.body.innerHTML += `<div>${planet.population}</div></div>`
     }
     console.log(allData);
-  }
-  catch (err) {
+
+    // document.body.innerHTML += `</div>`;
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
 getInfo();
